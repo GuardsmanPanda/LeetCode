b6fbcc5ebf980aae20b6965e89b44c7335976183
GuardsmanPanda <guardsmanpanda@gmail.com>
2024-03-17 20:20:32 +0000

    daily problem.

--- src/Problem/Problem57.java
+++ src/Problem/Problem57.java
@@ -5,36 +5,27 @@ import java.util.List;
 
 public class Problem57 {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new ArrayList<>();
-        boolean overlap = false;
-        for (int i = 0; i < intervals.length; i++) {
-            int[] cur = intervals[i];
-            if (cur[0] < newInterval[1] && cur[1] > newInterval[0]) {
-                newInterval[0] = Math.min(newInterval[0], cur[0]);
-                newInterval[1] = Math.max(newInterval[1], cur[1]);
-                while (i < intervals.length - 1 && intervals[i + 1][0] <= newInterval[1]) {
-                    cur = intervals[i + 1];
-                    newInterval[1] = Math.max(newInterval[1], cur[1]);
-                    i++;
+        List<int[]> result = new ArrayList<>();
+        boolean inserted = false;
+        for (int[] ii : intervals) {
+            if (ii[1] < newInterval[0]) {
+                result.add(ii);
+                continue;
+            }
+            if (ii[0] > newInterval[1]) {
+                if (!inserted) {
+                    result.add(newInterval);
+                    inserted = true;
                 }
-                overlap = true;
-                res.add(newInterval);
-            } else {
-                res.add(cur);
+                result.add(ii);
+                continue;
             }
+            newInterval[0] = Math.min(newInterval[0], ii[0]);
+            newInterval[1] = Math.max(newInterval[1], ii[1]);
         }
-        if (!overlap) {
-            for (int i = 0; i < res.size(); i++) {
-                if (res.get(i)[0] > newInterval[1]) {
-                    res.add(i, newInterval);
-                    overlap = true;
-                    break;
-                }
-            }
-            if (!overlap) {
-                res.add(newInterval);
-            }
+        if (!inserted) {
+            result.add(newInterval);
         }
-        return res.toArray(new int[0][]);
+        return result.toArray(new int[0][]);
     }
 }
