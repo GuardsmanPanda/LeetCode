084f0bd1a07b203aa2d21e7deb88636055a48cd8
GuardsmanPanda <guardsmanpanda@gmail.com>
2024-04-02 20:54:07 +0100

    daily problem.

--- src/Problem2400/Problem2444.java
+++ src/Problem2400/Problem2444.java
@@ -2,26 +2,23 @@ package Problem2400;
 
 public class Problem2444 {
     public long countSubarrays(int[] nums, int minK, int maxK) {
-        int minCount = 0, maxCount = 0, left1 = 0, left2 = 0;
-        long res = 0;
+        int lastMinK = -1, lastMaxK = -1, lastOutsideBound = -1;
+        long ans = 0;
         for (int i = 0; i < nums.length; i++) {
-            int n = nums[i];
-            if (n < minK || n > maxK) {
-                minCount = maxCount = 0;
-                left1 = left2 = i + 1;
-                continue;
+            int v = nums[i];
+            if (v < minK || v > maxK) {
+                lastOutsideBound = i;
+                lastMaxK = i;
+                lastMinK = i;
             }
-            minCount += n == minK ? 1 : 0;
-            maxCount += n == maxK ? 1 : 0;
-            while (left2 < i && nums[left2] != minK && nums[left2] != maxK || (nums[left2] == minK && minCount > 1) || (nums[left2] == maxK && maxCount > 1)) {
-                minCount -= nums[left2] == minK ? 1 : 0;
-                maxCount -= nums[left2] == maxK ? 1 : 0;
-                left2++;
+            if (v == minK) {
+                lastMinK = i;
             }
-            if (minCount > 0 && maxCount > 0) {
-                res += left2 - left1 + 1;
+            if (v == maxK) {
+                lastMaxK = i;
             }
+            ans += Math.min(lastMinK, lastMaxK) - lastOutsideBound;
         }
-        return res;
+        return ans;
     }
 }
